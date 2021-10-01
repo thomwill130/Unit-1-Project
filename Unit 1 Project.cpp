d876c43e9c5638b6eb0b43d9313a9640073bbe0c
William Thompson <ThomWill130@GoTVDSB.ca>
2021-10-01 10:54:03 -0400

    finished first structure for calculations

--- Unit 1 Project.cpp
+++ Unit 1 Project.cpp
@@ -6,32 +6,34 @@
 using namespace std;
 
 
-float volume, length, height, width, surfaceArea;
 
 
-void areaCalculate(float a, float b, float c)
+
+float areaCalculate(float a, float b, float c)
 {
-    volume = a * b * c;
-    cout << volume;
+    return a * b * c;
 }
-void perimeterCalculate(float a, float b, float c)
+float surfaceAreaCalculate(float a, float b, float c)
 {
-    surfaceArea = 2 * (c * a + b * a + b * c);
-    cout << surfaceArea;
+    return 2 * ((c * a) + (b * a) + (b * c));
 }
 
 int main()
 {
-    cout << "Hi and welcome to the renovation calculator. First, we will need to calculate the area and perimeter of the room. To do this, enter the length and height of the room. Enter the length below:\n";
+    float volume, length, height, width, surfaceArea;
+    cout << "Hi and welcome to the renovation calculator. First, we will need to calculate the area and perimeter of the room. To do this, enter the length and height of the room. Remember to use meters for all of these measurements. Enter the length below:\n";
     cin >> length;
-    cout << "\n\nNow enter the height of the room.\n";
+    cout << "\nNow enter the height of the room.\n";
     cin >> height;
-    cout << "\n\nFinaly, enter the width of the room.\n";
+    cout << "\nFinaly, enter the width of the room.\n";
     cin >> width;
-    cout << "\n\nThe area of your room is: ";
-    areaCalculate(length, width, height);
-    cout << "and the surface area is ";
-    perimeterCalculate(length, height, width);
-    cout <<". We will use these to calculate the cost of the renovation.\n\n"
+    cout << "\nThe area of your room is ";
+    volume = areaCalculate(length, width, height);
+    cout << volume;
+    cout << "m^2 and the surface area is ";
+    surfaceArea = surfaceAreaCalculate(length, height, width);
+    cout << surfaceArea;
+    cout << "m. We will use these to calculate the cost of the renovation.\n\n";
+}
 
 
