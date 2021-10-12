ebc4eb23763c2db724d5cacafb9016b09abe682c
William Thompson <ThomWill130@GoTVDSB.ca>
2021-10-12 10:54:56 -0400

    finsihe main eidjeruilh kdfcdj

--- Unit 1 Project.cpp
+++ Unit 1 Project.cpp
@@ -6,7 +6,8 @@
 using namespace std;
 
 
-
+//global variables
+float surfaceArea;
 
 //area calculator. a = length, b = height, c = width
 float areaCalculate(float a, float b, float c)
@@ -19,9 +20,14 @@ float surfaceAreaCalculate(float a, float b, float c, float d, float e)
     return 2 * (((c * a) + (b * a) + (b * c)) - (a * c) - (d * 9) - (e * 16));
 }
 
+float materialCostPreTax(float a)
+{
+    return 3 * a;
+}
 int main()
 {
-    float volume, length, height, width, surfaceArea, windowAmount, doorAmount;
+    //base calculations using return functions
+    float volume, length, height, width, windowAmount, doorAmount;
     cout << "Hi and welcome to the renovation calculator. First, we will need to calculate the area and perimeter of the room. To do this, enter the length and height of the room. Remember to use meters for all of these measurements. Enter the length below:\n";
     cin >> length;
     cout << "\nNow enter the height of the room.\n";
@@ -35,10 +41,14 @@ int main()
     cout << "\nThe area of your room is ";
     volume = areaCalculate(length, width, height);
     cout << volume;
-    cout << "m^2 and the surface area (that we will be painting) is ";
+    cout << "m^3 and the surface area (that we will be painting) is ";
     surfaceArea = surfaceAreaCalculate(length, height, width, windowAmount, doorAmount);
     cout << surfaceArea;
-    cout << "m. Keep in mind there is no need to include the surface area of the floor in this calculation, because we will not be painting the floor. So it has been removed. We will use these to calculate the cost of the renovation.\n\n";
+    cout << "m^2. Keep in mind there is no need to include the surface area of the floor in this calculation, because we will not be painting the floor. So it has been removed. We will use these to calculate the cost of the renovation.\n\n";
+    //i want to put a press enter to continue here and then cls for the extra mark for something we havent learned in class
+
+    //calculating materials needed for renovation and cost
+    cout << "Now since we have our calculations for the room, we know that we need to paint " << surfaceArea << " meters squared. Paint costs $3 per square foot. Therefore, our material cost pre-tax is $" << materialCostPreTax(surfaceArea) << ".\n";
 }
 
 
